e359deb1d9adb7623a94fe9a8ceff17ee42e8034
EduardoLR10 <dudulr10@gmail.com>
2024-11-27 12:01:44 -0300

    Add Nix infra

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs = inputs: with inputs; # pass through all inputs and bring them into scope
+
+    # Build the output set for each default system and map system sets into
+    # attributes, resulting in paths such as:
+    # nix build .#packages.x86_64-linux.<name>
+    flake-utils.lib.eachDefaultSystem (system:
+
+      # let-in expressions, very similar to Rust's let bindings.  These names
+      # are used to express the output but not themselves paths in the output.
+      let
+
+        # create nixpkgs that contains rustBuilder from cargo2nix overlay
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
+        };
+
+        # create the workspace & dependencies package set
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.75.0";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        # this is the output (recursive) set (expressed for each system)
+
+        # the packages in `nix build .#packages.<system>.<name>`
+        packages = {
+          # nix build .#rust-bitcoin-proxy
+          # nix build .#packages.x86_64-linux.rust-bitcoin-proxy
+          rust-bitcoin-proxy = (rustPkgs.workspace.rust-bitcoin-proxy {});
+          # nix build
+          default = packages.rust-bitcoin-proxy; # rec
+        };
+      }
+    );
+}
