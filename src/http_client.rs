bb003a2b2da0b780eb62f107ab15da06b567065d
EduardoLR10 <dudulr10@gmail.com>
2024-12-05 00:34:58 -0300

    Format

--- src/http_client.rs
+++ src/http_client.rs
@@ -1,4 +1,4 @@
-use reqwest::{Client, Url, StatusCode};
+use reqwest::{Client, StatusCode, Url};
 use serde_json::value::RawValue;
 use std::sync::atomic;
 
@@ -16,8 +16,8 @@ pub struct HttpClient {
 
 impl HttpClient {
     pub fn new(url: &str, user: &str, pass: &str) -> Result<HttpClient, Error> {
-        let parsed_url = Url::parse(url)
-            .map_err(|parse_error| {Error::Url(parse_error.to_string())})?;
+        let parsed_url =
+            Url::parse(url).map_err(|parse_error| Error::Url(parse_error.to_string()))?;
         Ok(HttpClient {
             client: Client::new(),
             url: parsed_url,
@@ -27,9 +27,11 @@ impl HttpClient {
         })
     }
 
-    pub async fn call_method(&self,
-                             method: &str,
-                             params: Option<Box<RawValue>>) -> Result<Response, Error> {
+    pub async fn call_method(
+        &self,
+        method: &str,
+        params: Option<Box<RawValue>>,
+    ) -> Result<Response, Error> {
         let request = Request {
             jsonrpc: "2.0",
             id: Some(self.nonce.fetch_add(1, atomic::Ordering::Relaxed)),
@@ -37,13 +39,16 @@ impl HttpClient {
             params: params.as_deref(),
         };
 
-        let http_response = self.client.post(self.url.clone())
+        let http_response = self
+            .client
+            .post(self.url.clone())
             .basic_auth(self.user.clone(), Some(self.pass.clone()))
             .json(&request)
             .header("content-type", "application/json")
-            .send().await?;
+            .send()
+            .await?;
 
-	// TODO: Better error treatment for other statuses code
+        // TODO: Better error treatment for other statuses code
         if http_response.status() == StatusCode::OK {
             // TODO: check response nonce is equal to request nonce.
             // TODO: check response jsonrpc version.
