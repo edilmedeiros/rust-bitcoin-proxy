d139eb0132bd6cfceceea81670c8f6d5ca2bb4c1
EduardoLR10 <dudulr10@gmail.com>
2024-12-11 23:24:03 -0300

    Use methods and params from roxy-cli in proxy endpoint in roxyd

--- src/roxyd/main.rs
+++ src/roxyd/main.rs
@@ -2,32 +2,28 @@ use roxy::error::*;
 use roxy::http_client::HttpClient;
 // use serde_json::value::RawValue;
 // use std::env;
-use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{post, web, App, HttpResponse, HttpServer};
+use roxy::json_rpc::*;
 use std::sync::Arc;
 
 // TODO: share client between services
 #[post("/proxy")]
-async fn proxy(client: web::Data<HttpClient>) -> impl Responder {
+async fn proxy(
+    json_rpc: web::Json<RpcRequest>,
+    client: web::Data<HttpClient>,
+) -> Result<HttpResponse, Error> {
     // TODO: add proper error handling
 
-    // args[1]: rpc user
-    // args[2]: rpc password
-    // TODO: proper user input
-
     // Steps
-    // We need to get the body of the request (comes from roxy-cli)
     // Consult the read payload and do some verification
     // Use username and password from file (read the file in main)
-    // Use a client for bitcoind (spawned in main)
-    // Call method from the read body of the request (comes from roxy-cli)
-    // Get response from bitcoind
-    // Extract JSON and pipe it back to roxy-cli
+    // Check response from bitcoind to treat it back to roxy-cli
 
-    let response = client.call_method("baz", None).await.unwrap();
+    let response = client
+        .call_method(&json_rpc.method, json_rpc.params.clone())
+        .await?;
 
-    println!("{}", serde_json::to_string(&response.clone()).unwrap());
-
-    HttpResponse::Ok().body(serde_json::to_string(&response.clone()).unwrap())
+    Ok(HttpResponse::Ok().body(serde_json::to_string(&response.clone()).unwrap()))
 }
 
 #[tokio::main]
@@ -37,13 +33,14 @@ async fn main() -> Result<(), Error> {
 
     let client = Arc::new(HttpClient::new("http://127.0.0.1:38332", "foo", "bar").unwrap());
 
-    let _ = HttpServer::new(move ||
-                            App::new()
-                            .app_data(web::Data::new(client.clone()))
-                            .service(proxy))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await;
+    let _ = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(client.clone()))
+            .service(proxy)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await;
 
     Ok(())
 }
