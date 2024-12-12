3bf032d7698544ed26d238967b55a96d5a2f6789
EduardoLR10 <dudulr10@gmail.com>
2024-12-11 23:22:53 -0300

    Update string references to use String

--- src/http_client.rs
+++ src/http_client.rs
@@ -31,12 +31,13 @@ impl HttpClient {
         &self,
         method: &str,
         params: Option<Box<RawValue>>,
-    ) -> Result<Response, Error> {
-        let request = Request {
-            jsonrpc: "2.0",
-            id: Some(self.nonce.fetch_add(1, atomic::Ordering::Relaxed)),
-            method: method,
-            params: params.as_deref(),
+    ) -> Result<RpcResponse, Error> {
+        let request_id = self.nonce.fetch_add(1, atomic::Ordering::Relaxed);
+        let request = RpcRequest {
+            jsonrpc: "2.0".to_owned(),
+            id: Some(request_id),
+            method: method.to_owned(),
+            params,
         };
 
         let http_response = self
@@ -49,12 +50,45 @@ impl HttpClient {
             .await?;
 
         // TODO: Better error treatment for other statuses code
-        if http_response.status() == StatusCode::OK {
-            // TODO: check response nonce is equal to request nonce.
-            // TODO: check response jsonrpc version.
-            Ok(serde_json::from_str(&http_response.text().await?)?)
-        } else {
-            Err(Error::Err(format!("{:?}", http_response)))
+        match http_response.status() {
+            StatusCode::OK => {
+                let payload = http_response.json::<RpcResponse>().await?;
+                let expected_version = "2.0".to_owned();
+                match (
+                    payload
+                        .id
+                        .as_ref()
+                        .map(|value| value.as_u64().map(|v| v as usize))
+                        .flatten(),
+                    payload.jsonrpc.as_ref(),
+                ) {
+                    // Before bitcoind v28.0, this match will probably fail.
+                    (Some(id), Some(version))
+                        if id == request_id && expected_version == *version =>
+                    {
+                        Ok(payload)
+                    }
+                    (Some(id), Some(_)) if id != request_id => Err(Error::Err(
+                        "Mismatched IDs between request and response".to_owned(),
+                    )),
+                    (None, _) => Err(Error::Err("Response lacks ID".to_owned())),
+                    (_, None) => Err(Error::Err("Response lacks JSON RPC Version".to_owned())),
+                    (_, Some(_)) => Err(Error::Err("Wrong JSON RPC Version".to_owned())),
+                }
+            }
+            StatusCode::NO_CONTENT => Err(Error::Err("No content".to_owned())),
+            StatusCode::BAD_REQUEST => Err(Error::Err("Bad request".to_owned())),
+            StatusCode::UNAUTHORIZED => Err(Error::Err("Unauthorized".to_owned())),
+            StatusCode::FORBIDDEN => Err(Error::Err("Forbidden".to_owned())),
+            StatusCode::NOT_FOUND => Err(Error::Err("Not found".to_owned())),
+            StatusCode::METHOD_NOT_ALLOWED => Err(Error::Err("Method Not Allowed".to_owned())),
+            StatusCode::INTERNAL_SERVER_ERROR => {
+                Err(Error::Err("Internal Server Error".to_owned()))
+            }
+            StatusCode::SERVICE_UNAVAILABLE => Err(Error::Err("Service Unavailable".to_owned())),
+            // The above are the available bitcoind statuses
+            // https://github.com/bitcoin/bitcoin/blob/d6b225f1652526cb053ec32c8ff09160d5a759c5/src/rpc/protocol.h#L10
+            _ => Err(Error::Err("Unreachable from Bitcoind".to_owned())),
         }
     }
 }
