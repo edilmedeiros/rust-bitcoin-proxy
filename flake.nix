3594a15144d1ea10cf00966b58bd2d3faac06ad6
EduardoLR10 <dudulr10@gmail.com>
2025-06-11 23:42:59 -0300

    Upgrade nix infra to use rust-flake

--- flake.nix
+++ flake.nix
@@ -1,86 +1,23 @@
 {
   inputs = {
-    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
-    flake-utils.follows = "cargo2nix/flake-utils";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
-    # Pinning latest nixpkgs (as of 18/12/2024) to pin Bitcoin dependency to v28
-    newNixpkgs.url = "github:NixOS/nixpkgs?rev=4f0dadbf38ee4cf4cc38cbc232b7708fddf965bc";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    systems.url = "github:nix-systems/default";
+    rust-flake.url = "github:juspay/rust-flake";
+    rust-flake.inputs.nixpkgs.follows = "nixpkgs";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.flake = false;
   };
 
-  outputs = inputs: with inputs; # pass through all inputs and bring them into scope
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-    # Build the output set for each default system and map system sets into
-    # attributes, resulting in paths such as:
-    # nix build .#packages.x86_64-linux.<name>
-    flake-utils.lib.eachDefaultSystem (system:
-
-      # let-in expressions, very similar to Rust's let bindings.  These names
-      # are used to express the output but not themselves paths in the output.
-      let
-
-        newPkgs = import newNixpkgs {
-          inherit system;
-        };
-        
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ cargo2nix.overlays.default ];
-        };
-
-        # create the workspace & dependencies package set
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.75.0";
-          packageFun = import ./Cargo.nix;
-          packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++ [    
-            (pkgs.rustBuilder.rustLib.makeOverride {
-              name = "openssl-sys";
-              overrideAttrs = drv: {
-                propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [
-                  pkgs.openssl
-                  pkgs.pkg-config
-                ];
-              };
-            })
-          ];
-          extraRustComponents = ["clippy" "rust-analyzer" "rustfmt"];
-        };
-
-        workspaceShell = (rustPkgs.workspaceShell {
-          packages = with pkgs; [ 
-            just
-            newPkgs.bitcoin
-          ];
-          shellHook = ''
-             echo -e "\\033[1;31m"Skipping bitcoind download..."\\033[0;m"
-             export BITCOIND_SKIP_DOWNLOAD=true
-          '';
-        });
-
-      in rec {
-        # this is the output (recursive) set (expressed for each system)
-
-        # the packages in `nix build .#packages.<system>.<name>`
-        packages = {
-          # nix build .#rust-bitcoin-proxy
-          # nix build .#packages.x86_64-linux.rust-bitcoin-proxy
-          roxy = (rustPkgs.workspace.roxy {});
-          # nix build
-          default = packages.roxy;
-        };
-
-        apps = {
-          inherit system;
-          roxyd = {
-            type = "app";
-            program ="${self.packages.${system}.roxy}/bin/roxyd";
-          };
-          roxy-cli = {
-            type = "app";
-            program ="${self.packages.${system}.roxy}/bin/roxy-cli";
-          };
-        };
-        
-        devShell = workspaceShell;       
-      }
-    );
+      # See ./nix/modules/*.nix for the modules that are imported here.
+      imports = with builtins;
+        map
+          (fn: ./nix/${fn})
+          (attrNames (readDir ./nix));
+    };
 }
