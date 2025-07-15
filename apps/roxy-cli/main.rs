5cbcde72d621dca34e2244f7da29ae9d29764968
EduardoLR10 <dudulr10@gmail.com>
2025-07-14 23:02:27 -0300

    Update roxy implementations to use HTTPS

--- apps/roxy-cli/main.rs
+++ apps/roxy-cli/main.rs
@@ -3,13 +3,25 @@ use clap::Parser;
 use cli::Cli;
 use cli::Commands;
 use roxy::rpc::types::*;
+use std::fs::File;
+use std::io::Read;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let cli = Cli::parse();
 
-    let roxyd_client = reqwest::Client::new();
-    let address = "http://localhost:8080/proxy";
+    let mut buf = Vec::new();
+    File::open("config/cert.pem")
+        .unwrap()
+        .read_to_end(&mut buf)
+        .unwrap();
+    let cert = reqwest::Certificate::from_pem(&buf)?;
+
+    let roxyd_client = reqwest::Client::builder()
+        .add_root_certificate(cert)
+        .build()?;
+
+    let address = "https://localhost:8080/proxy";
 
     match &cli.command {
         Commands::GetBlockchainInfo => {
