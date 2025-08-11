ededd3a3ef9801515df417019b3b881de37ff78d
EduardoLR10 <dudulr10@gmail.com>
2025-08-11 20:49:31 -0300

    Automate certification creation via nix hook

--- nix/bitcoin.nix
+++ nix/bitcoin.nix
@@ -17,6 +17,11 @@
           echo -e "\\033[1;31m"Skipping bitcoind download..."\\033[0;m"
           export BITCOIND_SKIP_DOWNLOAD=true
           export BITCOIND_EXE=${pkgs.bitcoin}/bin/bitcoind
+          test ! -e certs/key.pem && test ! -e certs/cert.pem && (cd certs && sh generate_certs.sh)
+          export ROXYD_TLS_CERT_PATH=certs/cert.pem
+          export ROXYD_TLS_KEY_PATH=certs/key.pem
+          export ROXYCLI_TLS_CERT_PATH=certs/cert.pem
+          export ROXYCLI_TLS_KEY_PATH=certs/key.pem
         '';
       };
     };
